bece30593d51fc246ec63cc2ea4c0bd248c38213
Doohwancho <doohwancho526@gmail.com>
2022-07-29 06:36:33 +0000

    crawl jobkorea multiple pages

--- crawl-job-korea.js
+++ crawl-job-korea.js
@@ -1,18 +1,25 @@
 const axios = require('axios'); //to crawl html page
 const cheerio = require('cheerio'); //light version of jquery
 
-const getHTML = async(keyword) => {
-    try {
-        const html = (await axios.get(`https://www.jobkorea.co.kr/Search/?stext=${encodeURI(keyword)}&careerType=1%2C4`)).data; //tip! await을 ()한번 더 감싸기!
+const jobs = [];
+
+const initializeMap = (pages) => {
+    for(let i = 0; i < pages; i++){
+        jobs.push(`https://www.jobkorea.co.kr/Search/?stext=`);
+    }
+}
+
+const getHTML = async(url, keyword) => {
+    try { 
+        const html = (await axios.get(url+encodeURI(keyword)+'&careerType=1%2C4')).data; //tip! await을 ()한번 더 감싸기!
         return html
     } catch(e) {
         console.log(e);
     }
 }
 
-const parsing = async(page) => {
+const parsing = (page) => {
     const $ = cheerio.load(page);
-    const jobs = [];
     const $jobList = $('.post');
     $jobList.each((idx, node) => {
         const jobTitle = $(node).find('.title:eq(0)').text().trim();
@@ -24,8 +31,6 @@ const parsing = async(page) => {
         const dueDate = $(node).find('.date:eq(0)').text().trim();
         const etc = $(node).find('.etc:eq(0)').text().trim();
 
-        
-
         jobs.push({
             jobTitle, company, experience, education, regularYN, region, dueDate, etc
         });
@@ -34,11 +39,23 @@ const parsing = async(page) => {
     return jobs;
 }
 
-const getJob = async(keyword) => {
-    const html = await getHTML(keyword);
-    const jobs = await parsing(html);
-    console.log(jobs);
-    console.log(jobs.length);
+
+const getJobEachPage = async(url, keyword) => {
+    return await getHTML(url, keyword).then((html) => parsing(html));
 }
 
-getJob('spring');
+
+const getJobIteratePages = async(keyword) => {
+    const promises = jobs.map(url => getJobEachPage(url, keyword));
+    await Promise.all(promises);
+    console.log(jobs);
+    console.log(`total number of job offers: ${jobs.length}`);
+}
+
+const main = async(keyword, pages) => {
+    initializeMap(pages);
+    getJobIteratePages(keyword);
+}
+
+main('node', 2);
+
