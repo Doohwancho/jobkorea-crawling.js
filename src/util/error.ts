2eb60db4bb976ebdf2981dfe08815ab829df83d0
Doohwancho <doohwancho526@gmail.com>
2022-08-29 22:28:24 +0900

    refactor: throw new Error() instead of console.log()

--- src/util/error.ts
+++ src/util/error.ts
@@ -1,3 +1,3 @@
 export const errorParameterNone = () =>  {
-    console.log(`exit(001): please insert parameter. ex. node main.js 스프링 7`);
+    throw new Error(`exit(001): please insert parameter. ex. node main.js 스프링 7`);
 }
